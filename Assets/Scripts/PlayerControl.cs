b2cc5ce7083dee6db253bf186ffbaaef9ee953e3
Ceren <ceren-yilmaz@hotmail.com>
2019-10-31 01:12:54 +0300

    init repo

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerControl : NetworkBehaviour
+{
+    public float upForce;
+    private bool isDead = false;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    [ClientCallback]
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            rb2d.velocity = Vector2.zero;
+            isDead = true;
+            GameController.instance.BirdDied();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<PlayerControl>().enabled = true;
+
+    }
+
+}
+
